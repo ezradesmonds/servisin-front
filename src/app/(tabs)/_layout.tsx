43a752cecd385bcaec19cd7235888e93659e9263
natanieljoshe <natanieljoshe@gmail.com>
2026-06-05 10:01:14 +0700

    feat: add tabs for navigation

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { View } from 'react-native';
+import { BottomnavbarHome } from '../../components/user/BottomnavbarHome';
+import { HeaderTop } from '../../components/user/HeaderTop';
+
+export default function TabsLayout() {
+  return (
+    <View style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
+      <HeaderTop />
+      <Tabs
+        tabBar={() => <BottomnavbarHome />}
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen name="home" />
+        <Tabs.Screen name="discover" />
+        <Tabs.Screen name="chat" />
+        <Tabs.Screen name="bookings" />
+        <Tabs.Screen name="profile" />
+      </Tabs>
+    </View>
+  );
+}
